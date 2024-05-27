def9c8de501d4d669bb47ea1590caa4fb4056d8e
Edos Ddos <ekaramyan@gmail.com>
2024-05-28 00:59:43 +0400

    editing users from cells

--- src/components/UI/CellModal.jsx
+++ src/components/UI/CellModal.jsx
@@ -11,7 +11,12 @@ import {
 	MenuItem,
 } from '@mui/material'
 import { Add, Clear, Close, Done } from '@mui/icons-material'
+import useUsers from '@/hooks/useUsers'
 import AuthButton from './AuthButton'
+import EditModal from './EditModal'
+import acceptImage from '@/assets/img/confirm.svg'
+import deleteImage from '@/assets/img/delete.svg'
+import leaveImage from '@/assets/img/leave.svg'
 
 export default function CellModal({
 	open,
@@ -43,6 +48,27 @@ export default function CellModal({
 		...Array(6 - trimmedFollowers.length).fill({}),
 	]
 	const [inputValues, setInputValues] = useState(Array(6).fill(null))
+	const [modalContent, setModalContent] = useState({
+		title: '',
+		imageSrc: null,
+	})
+	const [userFormData, setUserFormData] = useState({
+		firstName: '',
+		lastName: '',
+		birth: '',
+		email: '',
+		country: '',
+		nickname: '',
+		phone: '',
+		telegram: '',
+		additionalEmail: '',
+		additionalTelegram: '',
+		isBlocked: Boolean(false),
+	})
+	const [editModalOpen, setEditModalOpen] = useState(false)
+
+	const { data, loading, success, editUser, getUserById } = useUsers()
+
 	const handleChange = index => event => {
 		const newInputValues = [...inputValues]
 		newInputValues[index] = event.target.value
@@ -79,158 +105,245 @@ export default function CellModal({
 		consultant => consultant.id === formData.consultantId
 	)
 
-	console.log(cellData)
+	const handleOpenEditModal = async (action, actionType, id) => {
+		updateModalContent(actionType, id)
+		const { data } = await getUserById(id)
+		setUserFormData({
+			firstName: data?.firstName || '',
+			lastName: data?.lastName || '',
+			birth: data?.birth || '',
+			email: data?.email || '',
+			country: data?.country || '',
+			nickname: data?.nickname || '',
+			phone: data?.phone || '',
+			telegram: data?.telegram || '',
+			additionalEmail: data?.additionalEmail || '',
+			additionalTelegram: data?.additionalTelegram || '',
+			joinLimitBeginner: data?.joinLimitBeginner,
+			joinLimitWorker: data?.joinLimitWorker,
+			isBlocked: Boolean(data?.isBlocked),
+			isConfirmed: Boolean(data?.isConfirmed),
+		})
+		setEditModalOpen(true)
+	}
+
+	const updateModalContent = (action, id) => {
+		let content = {}
+		switch (action) {
+			case 'edit':
+				content = {
+					id: id,
+					title: `Editing user #${id}`,
+					imageSrc: acceptImage.src,
+				}
+				break
+			case 'delete':
+				content = {
+					title: `Are you sure you want to delete user #${id}?`,
+					imageSrc: deleteImage.src,
+				}
+				break
+			case 'leave':
+				content = {
+					title: 'Are you sure you want to leave?',
+					imageSrc: leaveImage.src,
+				}
+				break
+			default:
+				content = { title: '', imageSrc: null }
+		}
+		setModalContent(content)
+	}
+	const onEditClick = async (id, formData) => {
+		await editUser(id, formData)
+	}
 
 	return (
-		<Modal open={open} onClose={handleClose}>
-			<div
-				style={{
-					position: 'absolute',
-					top: '50%',
-					left: '50%',
-					transform: 'translate(-50%, -50%)',
-					backgroundColor: 'white',
-					padding: 20,
-					borderRadius: 5,
-					display: 'flex',
-					flexDirection: 'column',
-					gap: 10,
-				}}
-			>
-				<IconButton
-					onClick={handleClose}
-					style={{ position: 'absolute', top: 5, right: 5 }}
-				>
-					<Close />
-				</IconButton>
-				<h2>{title}</h2>
+		<>
+			<Modal open={open} onClose={handleClose}>
 				<div
 					style={{
-						display: 'grid',
-						gridTemplateColumns: '6.5fr 6.5fr 2fr',
-						width: '100%',
-						gap: 15,
+						position: 'absolute',
+						top: '50%',
+						left: '50%',
+						transform: 'translate(-50%, -50%)',
+						backgroundColor: 'white',
+						padding: 20,
+						borderRadius: 5,
+						display: 'flex',
+						flexDirection: 'column',
+						gap: 10,
 					}}
 				>
-					{/* <Typography variant='outlined'>
-						Consultant: {cellData.consultant.firstName}{' '}
-            {cellData.consultant.lastName}
-					</Typography> */}
-
-					<TextField
-						label='Consultant'
-						variant='outlined'
-						value={formData.consultantId}
-						onChange={handleInputChange}
-						fullWidth
-						select
-						name='consultantId'
+					<IconButton
+						onClick={handleClose}
+						style={{ position: 'absolute', top: 5, right: 5 }}
 					>
-						{consultants?.data?.map(consultant => (
-							<MenuItem key={consultant.id} value={consultant.id}>
-								{consultant.firstName} {consultant.lastName}
-							</MenuItem>
-						))}
-					</TextField>
-
-					<TextField
-						variant='outlined'
-						type='number'
-						value={formData?.leaderId || cellData.leader.id}
-						onChange={handleInputChange}
-						name='leaderId'
-						label='Leader'
-					/>
-					<Button
-						style={{ color: '#119A48' }}
-						onClick={() => editCaL(cellData.id, formData)}
+						<Close />
+					</IconButton>
+					<h2>{title}</h2>
+					<div
+						style={{
+							display: 'grid',
+							gridTemplateColumns: '6.5fr 6.5fr 2fr',
+							width: '100%',
+							gap: 15,
+						}}
 					>
-						<Done />
-					</Button>
-				</div>
-				<Box style={{ display: 'flex', flexDirection: 'column', gap: 15 }}>
-					{paddedFollowers.map((user, index) => (
-						<>
-							<div
-								key={index}
-								style={{
-									display: 'grid',
-									gridTemplateColumns: '12fr 1fr 1fr',
-									width: '100%',
-								}}
-							>
-								{user.id ? (
-									<>
-										<Typography variant='outlined'>
-											{user.follower.nickname} id - #{user.follower.id}
-										</Typography>
-										{patchingLoading ? (
-											<CircularProgress />
-										) : (
-											<>
-												{user.isAccepted !== true && (
-													<Button
-														style={{ color: '#119A48' }}
-														onClick={() => onApproveClick(user.id)}
-													>
-														<Done />
-													</Button>
-												)}
-												<Button onClick={() => onDeleteUserClick(user.id)}>
-													<Clear />
-												</Button>
-											</>
-										)}
-										{patchingError && (
-											<Typography variant='error'>{patchingError}</Typography>
-										)}
-									</>
-								) : (
-									<>
-										<TextField
-											style={{ width: '100%' }}
-											type='number'
-											label={'id'}
-											value={inputValues[index]}
-											onChange={handleChange(index)}
-										/>
-										{addFollowerLoading ? (
-											<CircularProgress />
-										) : (
-											<Button
-												style={{ color: '#119A48' }}
+						{/* <Typography variant='outlined'>
+							Consultant: {cellData.consultant.firstName}{' '}
+							{cellData.consultant.lastName}
+						</Typography> */}
+
+						<TextField
+							label='Consultant'
+							variant='outlined'
+							value={formData.consultantId}
+							onChange={handleInputChange}
+							fullWidth
+							select
+							name='consultantId'
+						>
+							{consultants?.data?.map(consultant => (
+								<MenuItem key={consultant.id} value={consultant.id}>
+									{consultant.firstName} {consultant.lastName}
+								</MenuItem>
+							))}
+						</TextField>
+
+						<TextField
+							variant='outlined'
+							type='number'
+							value={formData?.leaderId || cellData.leader.id}
+							onChange={handleInputChange}
+							name='leaderId'
+							label='Leader'
+						/>
+						<Button
+							style={{ color: '#119A48' }}
+							onClick={() => editCaL(cellData.id, formData)}
+						>
+							<Done />
+						</Button>
+					</div>
+					<Box style={{ display: 'flex', flexDirection: 'column', gap: 15 }}>
+						{paddedFollowers.map((user, index) => (
+							<>
+								<div
+									key={index}
+									style={{
+										display: 'grid',
+										gridTemplateColumns: '12fr 1fr 1fr',
+										width: '100%',
+									}}
+								>
+									{user.id ? (
+										<>
+											<Typography
+												variant='outlined'
 												onClick={() =>
-													onAddUserClick(
-														cellData.id,
-														Number(inputValues[index])
+													handleOpenEditModal(
+														onEditClick,
+														'edit',
+														user.follower.id
 													)
 												}
+												sx={{ cursor: 'pointer' }}
 											>
-												<Add />
-											</Button>
-										)}
-										{addFollowerError ? (
-											<Typography variant='error'>
-												{addFollowerError}
+												{user.follower.nickname} id - #{user.follower.id}
 											</Typography>
-										) : (
-											<></>
-										)}
-									</>
-								)}
-							</div>
-						</>
-					))}
-				</Box>
-				{acceptedCount === 6 &&
-					(closeLoading ? (
-						<CircularProgress />
-					) : (
-						<AuthButton onClick={() => closeCell(cellData.id)}>
-							Close the cell
-						</AuthButton>
-					))}
-			</div>
-		</Modal>
+											{patchingLoading ? (
+												<CircularProgress />
+											) : (
+												<>
+													{user.isAccepted !== true && (
+														<Button
+															style={{ color: '#119A48' }}
+															onClick={() => onApproveClick(user.id)}
+														>
+															<Done />
+														</Button>
+													)}
+													<Button onClick={() => onDeleteUserClick(user.id)}>
+														<Clear />
+													</Button>
+												</>
+											)}
+											{patchingError && (
+												<Typography variant='error'>{patchingError}</Typography>
+											)}
+										</>
+									) : (
+										<>
+											<TextField
+												style={{ width: '100%' }}
+												type='number'
+												label={'id'}
+												value={inputValues[index]}
+												onChange={handleChange(index)}
+											/>
+											{addFollowerLoading ? (
+												<CircularProgress />
+											) : (
+												<Button
+													style={{ color: '#119A48' }}
+													onClick={() =>
+														onAddUserClick(
+															cellData.id,
+															Number(inputValues[index])
+														)
+													}
+												>
+													<Add />
+												</Button>
+											)}
+											{addFollowerError ? (
+												<Typography variant='error'>
+													{addFollowerError}
+												</Typography>
+											) : (
+												<></>
+											)}
+										</>
+									)}
+								</div>
+							</>
+						))}
+					</Box>
+					{acceptedCount === 6 &&
+						(closeLoading ? (
+							<CircularProgress />
+						) : (
+							<AuthButton onClick={() => closeCell(cellData.id)}>
+								Close the cell
+							</AuthButton>
+						))}
+				</div>
+			</Modal>
+			<EditModal
+				open={editModalOpen}
+				handleClose={() => {
+					setEditModalOpen(false)
+					setUserFormData({
+						firstName: '',
+						lastName: '',
+						birth: '',
+						email: '',
+						country: '',
+						nickname: '',
+						phone: '',
+						telegram: '',
+						additionalEmail: '',
+						additionalTelegram: '',
+					})
+				}}
+				id={modalContent.id}
+				handleConfirm={onEditClick}
+				formData={userFormData}
+				setFormData={setUserFormData}
+				title={modalContent.title}
+				isLoading={loading}
+			/>
+		</>
 	)
 }
